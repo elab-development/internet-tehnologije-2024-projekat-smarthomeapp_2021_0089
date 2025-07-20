b6acbc3f4d19bd7a6c228e1fd3bf4fa91336c6a7
tamara <tamaradrobnjakovic7@gmail.com>
2025-07-20 14:43:41 +0200

    added filtering devices by location

---
+++ frontend_novi/src/components/DevicesUpperBar.jsx
@@ -0,0 +1,65 @@
+import {
+  Select,
+  Box,
+  createListCollection
+} from '@chakra-ui/react';
+import { useState, useMemo } from 'react';
+
+export default function DevicesUpperBar({ locations, onLocationFilter }) {
+  const [selectedLocation, setSelectedLocation] = useState([]);
+  const [errors, setErrors] = useState({ location: false });
+
+  // kreira kolekciju samo kad se promeni locations
+  const locationCollection = useMemo(() => {
+  if (!locations || locations.length === 0) {
+    return createListCollection({ items: [] });
+  }
+  return createListCollection({
+    items: [
+      { label: "All locations", value: "" }, // ova stavka uklanja filter
+      ...locations.map(loc => ({
+        label: loc.name,
+        value: loc.location_id.toString()
+      }))
+    ]
+  });
+}, [locations]);
+
+
+  return (
+    <Box width="100%" mb={4} maxW="300px">
+      <Select.Root
+        collection={locationCollection}
+        value={selectedLocation}
+        onValueChange={(details) => {
+          setSelectedLocation(details.value);
+          setErrors(prev => ({ ...prev, location: false }));
+
+          const parsed = details.value ? parseInt(details.value) : null;
+          onLocationFilter(parsed);
+        }}
+        invalid={errors.location}
+      >
+        <Select.HiddenSelect />
+        <Select.Control>
+          <Select.Trigger>
+            <Select.ValueText placeholder="Filter by location" />
+          </Select.Trigger>
+          <Select.IndicatorGroup>
+            <Select.Indicator />
+          </Select.IndicatorGroup>
+        </Select.Control>
+        <Select.Positioner>
+          <Select.Content>
+            {locationCollection.items.map(loc => (
+              <Select.Item item={loc} key={loc.value}>
+                {loc.label}
+                <Select.ItemIndicator />
+              </Select.Item>
+            ))}
+          </Select.Content>
+        </Select.Positioner>
+      </Select.Root>
+    </Box>
+  );
+}
--- frontend_novi/src/pages/DevicesPage.jsx
+++ frontend_novi/src/pages/DevicesPage.jsx
@@ -1,24 +1,31 @@
-import { Box, Flex} from '@chakra-ui/react';
+import { Box, Flex } from '@chakra-ui/react';
 import { useEffect, useState, useCallback } from "react";
 import DevicesSideBar from '../components/DevicesSideBar';
 import ThermostatCard from '../components/cards/ThermostatCard';
 import LightbulbCard from '../components/cards/LightbulbCard';
 import DoorlockCard from '../components/cards/DoorlockCard';
 import AirpurifierCard from '../components/cards/AirpurifierCard';
+import DevicesUpperBar from '../components/DevicesUpperBar';
 
 
 
 function Devices() {
   const [devices, setDevices] = useState([]);
   const [deviceTypeFilter, setDeviceTypeFilter] = useState(null);
+  const [locationFilter, setLocationFilter] = useState(null);
+  const [locations, setLocations] = useState([]);
 
   const token = localStorage.getItem("access_token");
 
   const fetchDevices = useCallback(() => {
+    if (!token) return;
     let url = "http://localhost:8000/devices/";
-    if (deviceTypeFilter) {
-      url += `?device_type=${deviceTypeFilter}`;
-    }
+    const params = new URLSearchParams();
+
+    if (deviceTypeFilter) params.append("device_type", deviceTypeFilter);
+    if (locationFilter) params.append("location_id", locationFilter);
+
+    url += `?${params.toString()}`;
 
     fetch(url, {
       headers: { Authorization: `Bearer ${token}` }
@@ -29,11 +36,42 @@ function Devices() {
         console.error("Failed to load devices:", err);
         setDevices([]);
       });
-  }, [deviceTypeFilter, token]);
+  }, [deviceTypeFilter, locationFilter, token]);
+
 
   useEffect(() => {
     fetchDevices();
   }, [fetchDevices]);
+
+  useEffect(() => {
+    const fetchUserAndLocations = async () => {
+      if (!token) return;
+
+      try {
+        const userRes = await fetch("http://localhost:8000/users/me", {
+          headers: { Authorization: `Bearer ${token}` }
+        });
+
+        if (!userRes.ok) throw new Error("Failed to fetch user");
+
+        const userData = await userRes.json();
+
+        const locRes = await fetch(`http://localhost:8000/users/${userData.user_id}/locations`, {
+          headers: { Authorization: `Bearer ${token}` }
+        });
+
+        if (!locRes.ok) throw new Error("Failed to fetch locations");
+
+        const locData = await locRes.json();
+        setLocations(locData);
+      } catch (err) {
+        console.error(err.message);
+      }
+    };
+
+    fetchUserAndLocations();
+  }, [token]);
+
 
 
   const handleDeviceDeleted = (deletedId) => {
@@ -65,17 +103,27 @@ function Devices() {
 
 
 
-  return (<Flex wrap='wrap' rowGap={'5'}>
-    <DevicesSideBar onFilter={setDeviceTypeFilter} onDeviceAdded={handleDeviceAdded} />
-    <Flex wrap="wrap" gap={3} width="100%" flex="1" p={3}>
-      {devices.map(device => (
-        <Box key={device.device_id} minWidth="260px">
-          {renderDeviceCard(device)}
-        </Box>
-      ))}
-    </Flex>
+  return (
+    <Flex align="flex-start" width="100%">
 
-  </Flex>);
+      <DevicesSideBar
+        onFilter={setDeviceTypeFilter}
+        onDeviceAdded={handleDeviceAdded}
+      />
+
+      <Box flex="1" p={3}>
+        <DevicesUpperBar onLocationFilter={setLocationFilter} locations={locations} />
+
+        <Flex wrap="wrap" gap={3}>
+          {devices.map((device) => (
+            <Box key={device.device_id} minWidth="260px">
+              {renderDeviceCard(device)}
+            </Box>
+          ))}
+        </Flex>
+      </Box>
+    </Flex>
+  )
 
 
 }
